754495a59d963b63ca74dadbf27f008760ed19c3
Imane-code-ux <72796883+Imane-code-ux@users.noreply.github.com>
2021-03-21 12:10:53 -0400

    Update primes_opt.c

--- TP1/EX3/primes_opt.c
+++ TP1/EX3/primes_opt.c
@@ -1,4 +1,5 @@
 #include "header.h"
+#include<math.h>
 
 // ? Note: A static function in C is a function that has a scope that is
 // limited to its object file. This means that the static function is only
@@ -6,13 +7,22 @@
 // A function can be declared as static function by placing the static keyword
 // before the function name
 
-static bool    is_prime_opt(int n)
+
+int is_prime_opt(int n)
 {
-    // write is_prime function code here
+    if(n==2);
+        return 1;
+    for(int i=3,i<=sqrt(abs(n));i=i+2)
+        if(n%i==0)
+            return 0;
+    return 1;
 }
 
 
 void    get_primes_opt(int n)
 {
-    // write get_primes_opt function code here that use is_prime_opt function
+    for(int i=2;i<=n,i++){
+        if(is_prime(i)==1)
+            printf("%d\n",i);
+    }
 }
