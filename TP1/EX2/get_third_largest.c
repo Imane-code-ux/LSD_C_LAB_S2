97d03a372b6700cc0656bd56134e7a991020f11f
Imane-code-ux <72796883+Imane-code-ux@users.noreply.github.com>
2021-03-21 08:12:45 -0400

    update get_third_largest.c

--- TP1/EX2/get_third_largest.c
+++ TP1/EX2/get_third_largest.c
@@ -1,11 +1,37 @@
 #include "header.h"
 
-/*
-** Hint: for finding the largest third element in the array you don't need
-** to sort the whole array, only three variables is needed for this purpose
-** and the algorithm comlexity will be O(n).
-*/
-int     get_third_largest(int *tab, int size)
+int     get_third_largest()
 {
-        // write your algo here
+//declaration d'un tableau d'entiers de taille 1000
+
+int tab[1000];
+
+//remplissage du tableau par des valeurs aléatoires
+
+for(int i=0;i<=1000,i++)
+        tab[i]=rand();
+
+//affichage du tableau
+
+for (int i=0;i<=1000;i++)
+        printf("%d|",tab[i]);
+
+//calcul du 3ème élement de ce tableau sans algorithme de tri
+int first,second,third;
+first=-1;
+second=-1;
+third=-1;
+for(int i=0;i<=1000;i++)
+        if(tab[i]>first)
+                first=tab[i];
+for(int i=0;i<=1000;i++)
+        if(tab[i]>second && tab[i]<first)
+                second=tab[i];
+for(int i=0;i<=1000;i++)
+        if(tab[i]>third && tab[i]<second)
+                third=Tab[i];
+return third;
+}
+
+
 }
