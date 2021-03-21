19d74ea0f85bb2961acd512ef47f5a17a76cf6ab
Imane-code-ux <72796883+Imane-code-ux@users.noreply.github.com>
2021-03-21 07:21:42 -0400

    update iterative_fibo

--- TP1/EX1/iterative_fibo.c
+++ TP1/EX1/iterative_fibo.c
@@ -2,5 +2,11 @@
 
 int iterative_fibo(int n)
 {
-    // write your iterative_fibo code here
+    int c=0,f0=0,f1=1;
+    for(int i=2;i<=n;i++){
+        c=f0+f1;
+        f0=f1;
+        f1=c;
+    }
+    return c;
 }
