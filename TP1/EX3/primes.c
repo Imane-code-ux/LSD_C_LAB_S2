5cd637d7064cfde4a7aab168f31d8f7c7d5aed0e
Imane-code-ux <72796883+Imane-code-ux@users.noreply.github.com>
2021-03-21 12:15:16 -0400

    update primes.c

--- TP1/EX3/primes.c
+++ TP1/EX3/primes.c
@@ -9,11 +9,9 @@
 
 int is_prime(int n)
 {
-    if(n==0 || n==1);
-        return 0;
     if(n==2);
         return 1;
-    for(int i=3,i<=abs(n)-1;i++)
+    for(int i=2,i<=abs(n)-1;i++)
         if(n%i==0)
             return 0;
     return 1;
